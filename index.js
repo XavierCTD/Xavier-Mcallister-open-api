c26a96e8b0dcf250703017a2d26c40a904aeab99
XavierCTD <macfamlove01@gmail.com>
2024-09-05 18:59:44 -0400

    index.css

--- index.js
+++ index.js
@@ -3,17 +3,36 @@ const today = new Date();
 let thisDay = today.getFullYear();
 console.log(thisDay);
 
-async function fetchApi() {
-  try {
-    const response = await fetch('https://www.swapi.tech/api/people/1);
-        if(!response.ok) {
-          throw new error('Request Failed');
-        }
-    const data = await response.json();
-  console.log(data);
-  } catch (error) {
-    console.error('An Error Occurred:', error);
+fetch('https://www.swapi.tech/api/people/1', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    "message": "ok",
+    "result": {
+      "properties": {
+        "height": "172",
+        "mass": "77",
+        "hair_color": "blond",
+        "skin_color": "fair",
+        "eye_color": "blue",
+        "birth_year": "19BBY",
+        "gender": "male",
+        "created": "2024-09-05T22:18:57.542Z",
+        "edited": "2024-09-05T22:18:57.542Z",
+        "name": "Luke Skywalker",
+        "homeworld": "https://www.swapi.tech/api/planets/1",
+        "url": "https://www.swapi.tech/api/people/1"
+      },
+    "description": "A person within the Star Wars universe",
+    "_id": "5f63a36eee9fd7000499be42",
+    "uid": "1",
+    "__v": 0
   }
-}
-
-fetchApi();
+  }).then(res => {
+    return res.json()
+  })
+})
+.then(data => console.log(data))
+  .catch(error => console.log('ERROR'))
