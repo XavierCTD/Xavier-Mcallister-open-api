1e6ecbaed4f9a751eb235e85f4f72eb6d9472f87
XavierCTD <macfamlove01@gmail.com>
2024-09-04 20:36:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+// Current Year 
+const today = new Date();
+let thisDay = today.getFullYear();
+console.log(thisDay);
 
+async function fetchApi() {
+  try {
+    const response = await fetch('https://www.swapi.tech/api/people/1);
+        if(!response.ok) {
+          throw new error('Request Failed');
+        }
+    const data = await response.json();
+  console.log(data);
+  } catch (error) {
+    console.error('An Error Occurred:', error);
+  }
+}
+
+fetchApi();
