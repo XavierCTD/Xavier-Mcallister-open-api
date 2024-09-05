f8af0406cd2db9a1b681b6cff733965b9ce5501a
XavierCTD <macfamlove01@gmail.com>
2024-09-05 19:06:30 -0400

    index.js

--- index.js
+++ index.js
@@ -3,36 +3,18 @@ const today = new Date();
 let thisDay = today.getFullYear();
 console.log(thisDay);
 
-fetch('https://www.swapi.tech/api/people/1', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    "message": "ok",
-    "result": {
-      "properties": {
-        "height": "172",
-        "mass": "77",
-        "hair_color": "blond",
-        "skin_color": "fair",
-        "eye_color": "blue",
-        "birth_year": "19BBY",
-        "gender": "male",
-        "created": "2024-09-05T22:18:57.542Z",
-        "edited": "2024-09-05T22:18:57.542Z",
-        "name": "Luke Skywalker",
-        "homeworld": "https://www.swapi.tech/api/planets/1",
-        "url": "https://www.swapi.tech/api/people/1"
-      },
-    "description": "A person within the Star Wars universe",
-    "_id": "5f63a36eee9fd7000499be42",
-    "uid": "1",
-    "__v": 0
+// Creating API
+
+fetch('https://www.swapi.tech/api/people/1')
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Request failed');
   }
-  }).then(res => {
-    return res.json()
-  })
+  return response.json(); 
 })
-.then(data => console.log(data))
-  .catch(error => console.log('ERROR'))
+.then(data => {
+  console.log(data);
+})
+.catch(error => {
+  console.error('An error occurred:', error);
+});
