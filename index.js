dfc1ba3c21186e058f506941f132701f867b0914
XavierCTD <macfamlove01@gmail.com>
2024-09-07 20:26:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log(thisDay);
 
 // Creating API
 
-fetch('https://www.swapi.tech/api/people/1')
+/*fetch('https://www.swapi.tech/api/people/1')
 .then(response => {
   if (!response.ok) {
     throw new Error('Request failed');
@@ -17,4 +17,5 @@ fetch('https://www.swapi.tech/api/people/1')
 })
 .catch(error => {
   console.error('An error occurred:', error);
-});
+});*/
+
