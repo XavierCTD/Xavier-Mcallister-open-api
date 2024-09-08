f318068a0d6892e53a7178a04e01ea2b8cbbe247
XavierCTD <macfamlove01@gmail.com>
2024-09-07 21:12:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,33 +9,29 @@ console.log(thisDay);
 // First API
 
 fetch('https://www.swapi.tech/api/people/1')
-.then(response => response.json());
-.then(respositoryFetched => {
-  console.log(repositoryFetched);
-  const starWarsPeople = document.getElementById('starwars');
-  const projectList = starWarsPeople.querySelector('#people');
-  for (let = 0; 0 < repositoryFetched.length; i++){
-    let starwars = document.createElement('li');
-    starwars.innerText = repositoryFetched[i].name;
-    projectList.appendChild(starwars)
-  }
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Request failed');
+  } return response.json(); 
 })
-.catch(err => console.log(err))
-
+.then(first => {
+  console.log(first);
+})
+catch(error => {
+  console.error('An error occurred:', error);
+});
 
 
 // Second API
 
 fetch('https://www.swapi.tech/api/species/1')
- .then(res => res.json());
- .then(dataOne => {
-   console.log(dataOne);
-   const starWarSpecies = document.getElementById('starAPI');
-   const proName = starWarsSpecies.querySelector('#species');
-   for (let = 0; 0 < dataOne.length; i++){
-     let starAPI = document.createElement('li');
-     starAPI.innertext = dataOne[i].name;
-     proname.appendChild(starAPI)
-   }
- })
- .catch(err => console.log(err))
+ .then(res => {
+  if (!res.ok) {
+    throw new Error('Request failed);
+  } return res.json();
+  .then(second => {
+    console.log(second);
+  })
+  catch(error => {
+    console.error('An error occurred:', error);
+  });
