13fc238894ad640f3f1e2aaf38183e2e36fb2d54
XavierCTD <macfamlove01@gmail.com>
2024-09-09 18:06:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const today = new Date();
 let thisDay = today.getFullYear();
 console.log(thisDay);
 
-// Creating Multiple APIs
 
+// Creating API
 
-const fetchAll = async () => {
-    Promise.all([
-        await fetch('https://www.swapi.tech/api/people/1'),
-        await fetch('https://www.swapi.tech/api/species/1')
-    ]).then(links => {
-        const response1 = links[0];
-        const response2 = links[1];
-
-        timeData = response1.json();
-        functionData = response2.json();
-    })
-}
+fetch('https://www.swapi.tech/api/people/1')
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Request failed');
+  }
+ return response.json(); 
+})
+.then(data => {
+  console.log(data);
+})
+.catch(error => {
+  console.error('An error occurred:', error);
+});
